c31071bda7160e5a4830d5aff594fadc1b2bb698
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2022-02-03 17:24:07 -0800

    Restructured RainbowScreen
    Made screens run off of AbstractScreenHandler and made RainbowScreen to replace ScreenBooter.

--- src/main/java/puretech/rpgengine/Main.java
+++ src/main/java/puretech/rpgengine/Main.java
@@ -1,16 +1,11 @@
 package puretech.rpgengine;
 
-import puretech.rpgengine.display.ScreenBooter;
+import puretech.rpgengine.display.RainbowScreen;
 
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        ScreenBooter.makeScreenBooter().disp(); // Windowed 500x500
-//        ScreenBooter.makeScreenBooter(true).disp(); // Fullscreen
-//        ScreenBooter.makeScreenBooter(new int[] {1000,1000}).disp(); // Windowed ?x? (in this case 1000x1000)
-//        ScreenBooter.makeScreenBooter(Color.BLACK).disp(); // Black Windowed 500x500 (Can also pass in RGB, but if any value is 255 it will rainbow)
-//        ScreenBooter.makeScreenBooter(new int[] {1000,1000}, Color.BLACK).disp(); // Black Windowed ?x? (in this case 1000x1000)
-//        ScreenBooter.makeScreenBooter(true, Color.BLACK).disp(); // Black Fullscreen
+        new RainbowScreen("Rainbow", new int[]{1,1},Color.RED,true).disp();
     }
 }
